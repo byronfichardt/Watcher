b83171c815fd16766b7a7921299f1ef7cc274c13
byron Fichardt <byron@Byron-ltp.TREELINE.CO.ZA>
2022-11-22 13:30:55 +0100

    small update

--- resources/js/Components/Exception.vue
+++ resources/js/Components/Exception.vue
@@ -1,16 +1,15 @@
 <template>
-    <div class="bg-gradient-to-b from-gray-100 to-gray-100 border-b-4 p-5 pr-1 grid grid-cols-12">
+    <div class="bg-gradient-to-b from-gray-100 to-gray-100 border-b-4 p-2 pr-1 grid grid-cols-12">
         <div class=" col-span-8">
-             <span class="flex gap-4">
-                 <Link class="font-bold text-xl" :href="'/exception/' + exception.id">{{exception.message }}</Link>
-                 <span class="font-bold text-gray-400 mt-0.5">{{ exception.file }}</span>
-             </span>
-            <p class="text-xl">{{ exception.description }}</p>
-            <span class="flex gap-4">
-                <span class="px-2 py-2 flex align-center">{{ exception.last_occurred_at }}</span>
-                <span v-if="exception.new" class="px-2 py-1 rounded text-gray-500 bg-red-200 font-semibold text-sm align-center flex w-max mt-1 mb-2">
+            <span class= "w-full block">
+                <Link class="font-bold text-xl" :href="'/exception/' + exception.id">{{exception.message }}</Link>
+            </span>
+            <span class="font-bold text-gray-400 mt-0.5">{{ exception.file }} : {{ exception.line}}</span>
+            <span class="block">
+                <span v-if="exception.new" class="px-2 py-1 mr-2 rounded text-gray-500 bg-red-200 font-semibold text-sm align-center flex w-max mt-1 mb-2 float-left">
                      New Issue
                 </span>
+                <span class="pr-2 pt-1.5 float-left">{{ dayjs(exception.last_occurred_at).fromNow() }}</span>
             </span>
         </div>
         <div class="col-span-1">
@@ -31,6 +30,9 @@
 <script setup>
 import {defineProps} from 'vue';
 import {Link} from '@inertiajs/inertia-vue3';
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
+dayjs.extend(relativeTime);
 
 defineProps({
     exception: {
--- resources/js/Components/ExceptionDetailsList.vue
+++ resources/js/Components/ExceptionDetailsList.vue
@@ -1,28 +1,36 @@
 <template>
-    <div class=" col-span-12 pb-4 ml-2">
-        <div class="w-full flex justify-between">
-            <a class="font-bold text-xl" href="#">{{ exception.message }}</a>
-        </div>
-    </div>
-    <div class="bg-gray-100 border-b-4 border-gray-200 rounded-t-lg px-3 py-1 grid grid-cols-12">
-        <div class=" col-span-4 px-2">
-            <h5 class="text-lg font-bold">File</h5>
-        </div>
-        <div class="col-span-8 px-2">
-            <div class="grid grid-cols-12">
-                <div class="col-span-10 flex justify-between">
-                    <h5 class="text-lg font-bold px-2">Trace</h5>
-                    <h5 class="text-lg font-bold px-2">Line</h5>
-                </div>
-                <div class="col-span-2">
-
-                </div>
+    <div class=" border-gray-200 rounded-t-lg px-3 py-1 grid grid-cols-4 w-2/6">
+        <div class=" col-span-4 pb-4 ml-2">
+            <div class="w-full">
+                <a class="font-bold text-xl break-words" href="#">{{ exception.message }}</a>
+                <span class="font-bold text-gray-400 ml-4">{{ dayjs(exception.created_at).fromNow() }}</span>
             </div>
         </div>
     </div>
+    <div class="bg-gray-100 border-b-4 border-gray-200 rounded-t-lg px-3 py-1 grid grid-cols-12">
+        <div class="col-span-4 px-2">
+            <h5 class="text-lg font-bold">File</h5>
+        </div>
+        <div class="col-span-4 px-2 flex gap-4">
+            <h5 class="text-lg font-bold cursor-pointer" @click="stacktrace = true; payload = false" :class="stacktrace ? 'bg-gray-200' : ''">Stack Trace</h5>
+            <h5 class="text-lg font-bold cursor-pointer" @click="stacktrace = false; payload = true" :class="payload ? 'bg-gray-200' : ''">Payload</h5>
+        </div>
+    </div>
 
-    <div v-for="event in exception.events">
-        <ExceptionDetails :event="event" :exception="exception"></ExceptionDetails>
+    <div class="grid grid-cols-12" >
+        <div class="bg-gray-100 pr-1 col-span-4 overflow-hidden " >
+            <div class="w-full">
+                <div v-for="event in exception.events" class="text-lg border-b-4 p-3" :class="selectedEvent && selectedEvent.id === event.id ? 'bg-gray-200' : ''">
+                    <a @click="selectedEvent = event"  class="cursor-pointer">{{ exception.file }} : {{exception.line}}</a>
+                </div>
+            </div>
+        </div>
+        <div class="bg-gradient-to-b from-gray-100 to-gray-100 border-b-4 p-3 pr-1 col-span-8 overflow-hidden"  v-if="stacktrace">
+            <ExceptionDetails v-if="selectedEvent" :event="selectedEvent" :exception="exception"></ExceptionDetails>
+        </div>
+        <div class="bg-gradient-to-b from-gray-100 to-gray-100 border-b-4 p-3 pr-1 col-span-8 overflow-hidden"  v-if="payload">
+            <pre class="text-sm text-gray-500 font-mono">{{ selectedEvent.payload }}</pre>
+        </div>
     </div>
 </template>
 
@@ -30,7 +38,14 @@
 import ExceptionDetails from "@/Components/ExceptionDetails.vue";
 import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
+import {ref} from "vue";
 dayjs.extend(relativeTime);
+
+const selectedEvent = ref(null);
+
+const stacktrace = ref(true);
+
+const payload = ref(false);
 
 const props = defineProps({
     exception: {
